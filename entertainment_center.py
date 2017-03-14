d25ca3a71ba9b432d910ca3747f8e06ea66c3bde
bcko <gobcko@gmail.com>
2017-03-13 21:26:43 -0400

    added avatar movie class

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,4 +2,8 @@ import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "http://www.youtube.com/watch?v=-0ceBgWV8io")
+
+avatar.show_trailer()
